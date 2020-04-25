84abc9bf733906a68a8b8b7a348b65a217da0aa0
acdean <github@koogy.clara.co.uk>
2020-04-25 21:04:03 +0100

    Platform size now dynamic

--- src/main/java/me/acdean/ziqqurat/Floor.java
+++ src/main/java/me/acdean/ziqqurat/Floor.java
@@ -24,20 +24,18 @@ public class Floor {
 
     Logger logger = LoggerFactory.getLogger(Floor.class);
 
-    private static final int COUNT = 20;    // nunmber of platforms
-
-    // platforms are 100x100, stairs are 50x50 (so 10x10 and 5x5 in character squares)
     public static final int STEPS = 5;                      // 5 steps between platforms
     public static final int STEP_SIZE = 10;                 // basic block size
-    public static final int PLAT_SIZE = 5;                 // platforms are this many steps wide (odd)
-    public static final int SIZE = (PLAT_SIZE * STEP_SIZE); // platform is 15x15 blocks
 
+    // platforms physical size is STEP_SIZE * plat_size
+    // the random platform size determines the nnumber of platforms in the grid
+    // ie there are more 5x5 platforms than 21x21 platforms
     Main p;
-    // and there are up to 15x15 platforms with stairs between them
-    int[][] grid = new int[COUNT][COUNT];       // this is for the platforms
-    // so the characters squares are 15 + 5 + 15 + 5 + ... + 5 + 15
-    private static final int COUNT2 = (COUNT * PLAT_SIZE) + ((COUNT - 1) * STEPS);
-    int[][] floor = new int[COUNT2][COUNT2];    // this is for the floor
+    int plat_size;  // size of the platforms
+    int count;      // number of platforms in grid
+    int size;       // physical size of the platform
+    int[][] grid;   // this is for the platforms (depends on random count)
+    int[][] floor;  // this is for the floor
     int colour;
 
     Floor(Main p) {
@@ -47,10 +45,30 @@ public class Floor {
         p.randomSeed(seed);
         p.noiseSeed(seed);
 
+        // 1 - 19
+        // 2 - 19
+        // 3 - 19
+        // 4 - 19
+        // 5 - 19
+        // 6 - 19
+        // 7 - 19
+        // 8 - 19
+        
+        // platform size
+        p.random(100);
+        plat_size = 1 + 2 * (int)p.random(2, 12);   // random odd number 5 - 25
+        size = plat_size * STEP_SIZE;   // dimensions for the platform
+        // we want the overall size to be about 256x256 
+        count = 256 / plat_size;
+        grid = new int[count][count];
+        int count2 = (count * plat_size) + ((count - 1) * STEPS);
+        floor = new int[count2][count2];
+        println("plat_size", plat_size, "size", size, "count", count, "count2", count2);
+
         // random walk
         int rx = 0, ry = 0;
         for (int f = 0; f < (int)p.random(4, 8); f++) {
-            if (p.random(100) < 30) {
+            if (p.random(100) < 20) {
                 // reset position to origin (else continue)
                 rx = 0;
                 ry = 0;
@@ -63,14 +81,14 @@ public class Floor {
                 }
                 sum += xinc;
                 //println("random", xinc);
-                rx = Main.constrain(rx + xinc, 0, COUNT - 1);  // these are inclusive
+                rx = Main.constrain(rx + xinc, 0, count - 1);  // these are inclusive
                 // don't allow diagonals
                 if (xinc == 0) {
                     int yinc = (int)p.random(0, 2);
                     if (p.random(100) < 50) {
                         yinc = -yinc;  // -1, 0, 1
                     }
-                    ry = Main.constrain(ry + yinc, 0, COUNT - 1);
+                    ry = Main.constrain(ry + yinc, 0, count - 1);
                 }
                 grid[rx][ry] = 1;
             }
@@ -78,8 +96,8 @@ public class Floor {
         }
 
         // now heightmap the drawn cells
-        for (int y = 0; y < COUNT; y++) {
-            for (int x = 0; x < COUNT; x++) {
+        for (int y = 0; y < count; y++) {
+            for (int x = 0; x < count; x++) {
                 if (grid[x][y] != 0) {
                     grid[x][y] = (int)(15 * p.noise(x * .06f, y * .05f));
                     print(grid[x][y] % 10);
@@ -92,15 +110,15 @@ public class Floor {
 
         // normalise the grid
         int min = 999;
-        for (int y = 0; y < COUNT; y++) {
-            for (int x = 0; x < COUNT; x++) {
+        for (int y = 0; y < count; y++) {
+            for (int x = 0; x < count; x++) {
                 if (grid[x][y] != 0 && grid[x][y] < min) {
                     min = grid[x][y];
                 }
             }
         }
-        for (int y = 0; y < COUNT; y++) {
-            for (int x = 0; x < COUNT; x++) {
+        for (int y = 0; y < count; y++) {
+            for (int x = 0; x < count; x++) {
                 if (grid[x][y] != 0) {
                     grid[x][y] -= (min - 1);
                     // debug
@@ -122,8 +140,8 @@ public class Floor {
         p.fill(0);
         p.strokeWeight(3);
         p.stroke(colour);
-        for (int y = 0 ; y < COUNT ; y++) {
-            for (int x = 0 ; x < COUNT ; x++) {
+        for (int y = 0 ; y < count ; y++) {
+            for (int x = 0 ; x < count ; x++) {
                 if (grid[x][y] != 0) {
                     drawPlatform(x, y);
                 }
@@ -137,11 +155,11 @@ public class Floor {
         int sy = toY(y);
         int sz = toZ(x, y);
         p.translate(sx, sy, sz);
-        p.box(SIZE, SIZE, STEP_SIZE);
-        if (y < COUNT - 1) {
+        p.box(size, size, STEP_SIZE);
+        if (y < count - 1) {
             drawSteps(x, y, false);
         }
-        if (x < COUNT - 1) {
+        if (x < count - 1) {
             drawSteps(x, y, true);
         }
         p.popMatrix();
@@ -162,7 +180,7 @@ public class Floor {
             h = STEPS * STEP_SIZE;
             xinc = STEP_SIZE;   // difference between steps
             yinc = 0;
-            tx = (int)(xinc * ((PLAT_SIZE - 1) / 2));  // initial translation
+            tx = (int)(xinc * ((plat_size - 1) / 2));  // initial translation
             ty = 0;
         } else {
             ez = grid[x][y + 1];
@@ -174,7 +192,7 @@ public class Floor {
             xinc = 0;
             yinc = STEP_SIZE;
             tx = 0;
-            ty = (int)(yinc * ((PLAT_SIZE - 1) / 2));
+            ty = (int)(yinc * ((plat_size - 1) / 2));
         }
         if (Main.abs(sz - ez) > 1) {
             // stairs too steep - skip
@@ -184,7 +202,7 @@ public class Floor {
         zinc = STEP_SIZE * (ez - sz);
         if (sz == ez) {
             // flat platform
-            p.translate(xinc * (PLAT_SIZE + STEPS) / 2, yinc * (PLAT_SIZE + STEPS) / 2);  // hacky
+            p.translate(xinc * (plat_size + STEPS) / 2, yinc * (plat_size + STEPS) / 2);  // hacky
             p.box(STEPS * STEP_SIZE, STEPS * STEP_SIZE, STEP_SIZE);
         } else {
             p.translate(tx, ty, 0);
@@ -202,15 +220,15 @@ public class Floor {
     }
 
     final int toX(int x) {
-        return x * (Floor.SIZE + (Floor.STEPS * Floor.STEP_SIZE));
+        return x * (size + (STEPS * STEP_SIZE));
     }
 
     final int toY(int y) {
-        return y * (Floor.SIZE + (Floor.STEPS * Floor.STEP_SIZE));
+        return y * (size + (STEPS * STEP_SIZE));
     }
 
     final int toZ(int x, int y) {
-        return grid[x][y] * (Floor.STEPS + 1) * Floor.STEP_SIZE;
+        return grid[x][y] * (STEPS + 1) * STEP_SIZE;
     }
 
     final PVector position(int x, int y, int z) {
